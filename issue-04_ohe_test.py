b514a65fa0fae423be7194f796a3fdc533cee4c1
Dmitrii Spiridonov <spiridonovdmitry@mail.ru>
2022-11-20 13:21:34 +0300

    fixed once more mistake

--- issue-04_ohe_test.py
+++ issue-04_ohe_test.py
@@ -3,19 +3,23 @@ import pytest
 from one_hot_encoder import fit_transform
 
 def test_countries():
-    assert fit_transform(['Russia', 'USA', 'France', 'USA', 'Italy']) == [('Russia', [0, 0, 0, 1]),
-                                                                          ('USA', [0, 0, 1, 0]),
-                                                                          ('France', [0, 1, 0, 0]),
-                                                                          ('USA', [0, 0, 1, 0]),
-                                                                          ('Italy', [1, 0, 0, 0])]
+    actual = fit_transform(['Russia', 'USA', 'France', 'USA', 'Italy'])
+    expected = [('Russia', [0, 0, 0, 1]),
+                ('USA', [0, 0, 1, 0]),
+                ('France', [0, 1, 0, 0]),
+                ('USA', [0, 0, 1, 0]),
+                ('Italy', [1, 0, 0, 0])]
+    assert actual == expected
 
 
 def test_numbers():
-    assert fit_transform([1, 2, 3, 4, 2, 3]) == [(1, [0, 0, 0, 1]),
-                                                 (2, [0, 0, 1, 0]),
-                                                 (3, [0, 1, 0, 0]),
-                                                 (4, [1, 0, 0, 0]),
-                                                 (2, [0, 0, 1, 0])]
+    actual = fit_transform([1, 2, 3, 4, 2, 3])
+    expected = [(1, [0, 0, 0, 1]),
+                (2, [0, 0, 1, 0]),
+                (3, [0, 1, 0, 0]),
+                (4, [1, 0, 0, 0]),
+                (2, [0, 0, 1, 0])]
+    assert actual == expected
 
 
 def test_no_args_error():
